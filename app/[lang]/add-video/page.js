0e576d9af22b6f5b2817ceb919275c15dd97d32d
kursatsmsek <kursatsimsek@protonmail.ch>
2023-12-15 02:35:41 +0300

    add video page design

---
+++ app/[lang]/add-video/page.js
@@ -0,0 +1,8 @@
+import AddVideoContainer from "@/containers/add-video";
+import React from "react";
+
+function AddVideo() {
+  return <AddVideoContainer />;
+}
+
+export default AddVideo;
---
+++ containers/add-video/index.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+function AddVideoContainer() {
+  return (
+    <form class="max-w-sm mx-auto mt-28 p-4">
+      <div class="mb-5">
+        <label
+          for="email"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          <p class="mb-6 text-base leading-7 text-gray-500 dark:text-gray-400">
+            Sitede olmasını istediğiniz videoyu ekleyebilirsiniz. Sadece yüksek
+            kaliteli videolar onaylanır ve sergilenir. 🦁
+          </p>
+        </label>
+        <label
+          for="email"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Video Sahibi
+        </label>
+        <input
+          type="email"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Galatasaray"
+          required
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Video Linki
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Ör: youtube.com/watch?v=7YRTcNtv8pk"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Açıklama
+        </label>
+
+        <textarea
+          id="message"
+          rows="4"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Leave a comment..."
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Kategori
+        </label>
+
+        <select
+          id="countries"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option>Haberler</option>
+          <option>En Çok İzlenenler</option>
+          <option>Biyografik</option>
+          <option>Bahsedilenler</option>
+          <option>Diğer</option>
+        </select>
+      </div>
+
+      <button
+        type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Gönder
+      </button>
+    </form>
+  );
+}
+
+export default AddVideoContainer;
