6d734ab0cc429e3dec6cfc740af345e8b35e625e
kursatsmsek <kursatsimsek@protonmail.ch>
2024-01-01 21:43:24 +0300

    stats calls added to apiCalls

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -43,3 +43,133 @@ export async function clickIntroPlay() {
     console.error("Error:", error.message);
   }
 }
+
+export async function clickLogo() {
+  try {
+    await fetch(`${baseUrl}/statistic/LOGO`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickAddVideo() {
+  try {
+    await fetch(`${baseUrl}/statistic/ADD_VIDEO`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickFigureToy() {
+  try {
+    await fetch(`${baseUrl}/statistic/FIGURE_TOY`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickEnglishFlag() {
+  try {
+    await fetch(`${baseUrl}/statistic/ENGLISH_FLAG`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickTurkishFlag() {
+  try {
+    await fetch(`${baseUrl}/statistic/TURKISH_FLAG`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickSpanishFlag() {
+  try {
+    await fetch(`${baseUrl}/statistic/SPANISH_FLAG`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickVideoDetail() {
+  try {
+    await fetch(`${baseUrl}/statistic/VIDEO_DETAIL`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickCoffee() {
+  try {
+    await fetch(`${baseUrl}/statistic/COFFEE`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickInstagram() {
+  try {
+    await fetch(`${baseUrl}/statistic/INSTAGRAM`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickTwitter() {
+  try {
+    await fetch(`${baseUrl}/statistic/TWITTER`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickLinkedin() {
+  try {
+    await fetch(`${baseUrl}/statistic/LINKEDIN`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickGithub() {
+  try {
+    await fetch(`${baseUrl}/statistic/GITHUB`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function clickEmail() {
+  try {
+    await fetch(`${baseUrl}/statistic/EMAIL`, {
+      method: "PUT",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
