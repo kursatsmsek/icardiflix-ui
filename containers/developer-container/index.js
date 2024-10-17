4c900da3e7b2c4677c52813bf580fc57552b5e8d
kursatsmsek <kursatsimsek@protonmail.ch>
2024-10-17 18:36:52 +0300

    developer contact bg color was changed

--- containers/developer-container/index.js
+++ containers/developer-container/index.js
@@ -22,7 +22,7 @@ function DeveloperContactContainer({ lang, i18n }) {
                   alt="merry-christmas"
                   className="relative z-10 object-cover w-full rounded h-full"
                 />
-                <div className="absolute bottom-0 right-0 z-10 p-8 bg-white border-4 border-red-500 rounded shadow dark:border-red-400 lg:-mb-8 lg:-mr-11 sm:p-8 dark:text-gray-300 dark:bg-gray-800 ">
+                <div className="absolute bottom-0 right-0 z-10 p-8 bg-gray-600 border-4 border-red-500 rounded shadow dark:border-red-400 lg:-mb-8 lg:-mr-11 sm:p-8 dark:text-gray-300 dark:bg-gray-800 ">
                   <p className="text-sm font-semibold md:w-72">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
